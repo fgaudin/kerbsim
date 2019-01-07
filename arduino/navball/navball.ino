65a71aa76aa6d033202e11daebeb4bdb88f389b3
Francois Gaudin <contact@francoisgaudin.com>
2019-01-06 16:57:52 -0800

    Rewrote arduino part for navball

--- arduino/navball/navball.ino
+++ arduino/navball/navball.ino
@@ -1,24 +1,10 @@
-
-/*
-  Stepper Motor Control - one revolution
-
-  This program drives a unipolar or bipolar stepper motor.
-  The motor is attached to digital pins 8 - 11 of the Arduino.
-
-  The motor should revolve one revolution in one direction, then
-  one revolution in the other direction.
-
-
-  Created 11 Mar. 2007
-  Modified 30 Nov. 2009
-  by Tom Igoe
-
-*/
-
 #include <Stepper.h>
 #include <math.h>
 
-const char sep = '|';
+#define PIN_OVERFLOW 3
+#define PIN_PROCESS 2
+
+const float maxRpm = 15;
 const int internalStepsPerRev = 32;
 //const float internalGearRatio = 25792.0/405;
 const float internalGearRatio = 64;
@@ -29,84 +15,162 @@ const float stepsPerRevolution = internalStepsPerRev * internalGearRatio * exter
 // initialize the stepper library on pins 8 through 11:
 Stepper myStepper(internalStepsPerRev * internalGearRatio, 8, 10, 9, 11);
 
+const byte numChars = 32;
+char receivedChars[numChars];
+const char startMarker = '[';
+const char endMarker = ']';
+boolean packetReceived = false;
+
+float pitch = 0, heading = 0, roll = 0;
+float pitchRequested = 0, headingRequested = 0, rollRequested = 0;
+
+unsigned long lastTime;
+
 void setup() {
+  pinMode(PIN_OVERFLOW, OUTPUT);
+  pinMode(PIN_PROCESS, OUTPUT);
+  dataTest();
   myStepper.setSpeed(15);
-  // initialize the serial port:
   Serial.begin(9600);
-  Serial.println(internalStepsPerRev);
-  Serial.println(internalGearRatio);
-  Serial.println(externalGearRatio);
-  Serial.println(stepsPerRevolution, 6);
+    
+}
+
+void rateOk() {
+  digitalWrite(PIN_OVERFLOW, LOW);
+}
+
+void rateKo() {
+  digitalWrite(PIN_OVERFLOW, HIGH);
+}
+
+void dataTest() {
+  rateOk();
+  for(int i = 0; i<3; i++) {
+    rateKo();
+    delay(200);
+    rateOk();
+    delay(200);
+  }
 }
 
 void loop() {
+  receiveData();
+  parseData();
+}
 
-  char msg[17] = "";
-  char pitchStr[7] = "";
-  char rpmStr[6] = "";
-  int byte_read = 0;
-  float pitchCurrent = 0, pitchRequested, pitchDelta, pitchDeltaForward, pitchDeltaBackward, pitchDeltaAdjusted;
-  int stepsCurrent = 0, stepsAdjusted;
-  const int lf = 10;
-  float rpm;
-  
+void receiveData() {
+  static boolean recvInProgress = false;
+  static byte ndx = 0;
+  char rc;
 
-  while (1) {
-    memset(msg, 0, sizeof(msg));
-    memset(pitchStr, 0, sizeof(pitchStr));
-    memset(rpmStr, 0, sizeof(rpmStr));
-    int sepIndex = -1;
-
-    if (Serial.available() > 0) {
-      // read the incoming byte:
-      byte_read = Serial.readBytesUntil(lf, msg, 17);
-      for(int i = 0; i < byte_read; i++) {
-        if (msg[i] == sep) {
-          sepIndex = i;
-        } else if (sepIndex > -1) {
-          rpmStr[i-sepIndex-1] = msg[i];
-        } else {
-          pitchStr[i] = msg[i];
-        }
-      }
-      pitchRequested = atof(pitchStr);
-      rpm = atof(rpmStr);
-
-      Serial.print("received msg: ");
-      Serial.println(msg);
-      Serial.print("received angle: ");
-      Serial.println(pitchStr);
-      Serial.print("received rpm: ");
-      Serial.println(rpmStr);
-      myStepper.setSpeed(max(round(rpm), 1));
-
-      pitchDeltaForward = fmod(pitchRequested - pitchCurrent + 360, 360);
-      pitchDeltaBackward = pitchDeltaForward - 360;
-      if (abs(pitchDeltaForward) < abs(pitchDeltaBackward)) {
-        pitchDelta = pitchDeltaForward;
-      } else {
-        pitchDelta = pitchDeltaBackward;
-      }
-      // Serial.print("pitchRequested: ");
-      // Serial.println(pitchRequested, 2);
-      // Serial.print("pitchDelta: ");
-      // Serial.println(pitchDelta, 2);
-
-      stepsAdjusted = round(pitchDelta * stepsPerRevolution / 360);
-      // Serial.print("stepsAdjusted: ");
-      // Serial.println(stepsAdjusted);
-      stepsCurrent += stepsAdjusted;
-      // Serial.print("stepsCurrent: ");
-      // Serial.println(stepsCurrent);
-      pitchDeltaAdjusted = stepsAdjusted * 360.0 / stepsPerRevolution;
-      pitchCurrent = fmod(pitchCurrent + pitchDeltaAdjusted + 360, 360);
-      // Serial.print("pitchDeltaAdjusted: ");
-      // Serial.println(pitchDeltaAdjusted, 6);
-
-      // Serial.print("pitchCurrent: ");
-      // Serial.println(pitchCurrent, 6);
-
-      myStepper.step(stepsAdjusted);
+  while (Serial.available() > 0) {
+    if (packetReceived) {
+      // first packet hasn't be handled yet
+      // and we're already receiving another one
+      // discarding the previous one
+      rateKo();
+      packetReceived = false;
     }
+    rc = Serial.read();
+
+    if (recvInProgress == true) {
+      if (rc != endMarker) {
+        receivedChars[ndx] = rc;
+        ndx++;
+        if (ndx >= numChars) {
+            ndx = numChars - 1;
+        }
+      } else {
+        receivedChars[ndx] = '\0'; // terminate the string
+        recvInProgress = false;
+        ndx = 0;
+        packetReceived = true;
+      }
+    } else if (rc == startMarker) {
+      recvInProgress = true;
+    }
+  }
+}
+
+void parseData() {
+  if (packetReceived) {
+    char tempChars[numChars];
+    char * strtokIndx; // this is used by strtok() as an index
+
+    strcpy(tempChars, receivedChars);
+
+    strtokIndx = strtok(tempChars,"|");      // get the first part - the string
+    headingRequested = atof(strtokIndx);
+ 
+    strtokIndx = strtok(NULL, "|"); // this continues where the previous call left off
+    pitchRequested = atof(strtokIndx);
+
+    strtokIndx = strtok(NULL, "|");
+    rollRequested = atof(strtokIndx);
+
+    // we can let another packet come in
+    packetReceived = false;
+
+    process();
+    rateOk();
+  }
+}
+
+void process() {
+  digitalWrite(PIN_PROCESS, HIGH);
+  moveHeading();
+  digitalWrite(PIN_PROCESS, LOW);
+}
+
+float smallestAngle(float origin, float target) {
+  float delta;
+  delta = target - origin;
+  if (delta > 180) {
+    delta -= 360 ;
+  } else if (delta < -180) {
+    delta += 360;
+  }
+  return delta;
+}
+
+int angleToSteps(float angle) {
+  return round(angle * stepsPerRevolution / 360);
+}
+
+float stepsToAngle(int steps) {
+  return steps * 360.0 / stepsPerRevolution;
+}
+
+float rpm(float angle, float duration_sec) {
+  return abs((angle * 60 / duration_sec) / 360);
+}
+
+void moveHeading() {
+  float delta;
+  int stepsToMove;
+  float deltaQuantified;
+  unsigned long currentTime;
+  float secSinceLastMove;
+  int rpmNeeded;
+
+  if (headingRequested != heading) {
+    currentTime = millis();
+    secSinceLastMove = (currentTime - lastTime) / 1000;
+    lastTime = currentTime;
+
+    delta = smallestAngle(heading, headingRequested);
+    stepsToMove = angleToSteps(delta);
+    deltaQuantified = stepsToAngle(stepsToMove);
+    heading = fmod(heading + deltaQuantified + 360, 360);
+    rpmNeeded = rpm(deltaQuantified, secSinceLastMove);
+    rpmNeeded = 15;
+    if (rpmNeeded > maxRpm) {
+      rateKo();
+    } else {
+      rateOk();
+    }
+    rpmNeeded = constrain(1, rpmNeeded, maxRpm);
+    myStepper.setSpeed(rpmNeeded);
+    myStepper.step(stepsToMove);
   }
 }
--- python/navball.py
+++ python/navball.py
@@ -7,31 +7,21 @@ conn = krpc.connect(name='Pitch/Heading/Roll')
 vessel = conn.space_center.active_vessel
 
 ser = serial.Serial('COM3', 9600, timeout=10)
-sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-
-previous_heading = None
-previous_heading_time = None
-rpm = None
 
 while(True):
-    line = ser.readline()
-    while (line):
-        print(line)
-        line = ser.readline()
-
-    time.sleep(0.1)
+    time.sleep(0.5)
     heading = vessel.flight().heading
-    current_time = time.time()
-    if previous_heading is not None:    
-        delta_heading = (heading - previous_heading + 180) % 360 - 180
-        delta_time = current_time - previous_heading_time
-        rpm = abs((delta_heading * 60 / delta_time) / 360)
-    previous_heading = heading
-    previous_heading_time = current_time
-        
-    print('heading: {}'.format(heading))
-    print('rpm: {}'.format(rpm))
+    pitch = vessel.flight().pitch
+    roll = vessel.flight().roll
 
-    if rpm is not None:
-        sio.write(str(round(heading, 2)) + '|' + str(round(rpm, 2)))
-        sio.flush() # it is buffering. required to get the data out *now*
+    print('heading: {}'.format(heading))
+    print('pitch: {}'.format(pitch))
+    print('roll: {}'.format(roll))
+
+    packet = "[{}|{}|{}]".format(
+        round(heading, 2),
+        round(pitch, 2),
+        round(roll, 2),
+    )
+    ser.write(bytes(packet, 'utf-8'))
+
